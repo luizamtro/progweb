9cd2124355774b8f80459a0d13ff8cb04976bd75
Luiza O' Monteiro <159566275+luizamtro@users.noreply.github.com>
2025-05-21 00:40:42 -0300

    Add files via upload

---
+++ SiteIndiv/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const path = require('path');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Middleware express-ejs-layouts
+app.use(expressLayouts);
+
+// Define o layout padrão
+app.set('layout', 'layout');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rotas
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Inicio' });
+});
+
+app.get('/sobre', (req, res) => {
+  res.render('sobre', { title: 'Sobre' });
+});
+
+app.get('/contato', (req, res) => {
+  res.render('contato', { title: 'Contato' });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta: ${PORT}`);
+});
